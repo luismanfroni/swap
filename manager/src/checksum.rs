6a30d4895ed23f71a183a149806883d8c1624ad4
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2020-05-22 01:25:29 -0300

    chore: checksum functions

--- manager/src/checksum.rs
+++ manager/src/checksum.rs
@@ -1,7 +1,14 @@
-use sha1::{Sha1, Digest};
+use blake3::{ hash, Hash };
 use std::fs;
 
-pub fn checksum(String path) {
-    let mut file = fs::File::open(&path)?;
-    return Sha1::digest_reader(&mut file)?
+pub fn checksum_from_file(String filepath): String {
+    let mut file: fs::File = fs::File::open(&path)?;
+    let bytes: Vec<u8> = file.bytes().collect();
+    return checksum(bytes);
+}
+
+pub fn checksum(bytes: Vec<u8>): String {
+    let hashed: Vec<u8> = hash(bytes).as_bytes().into_vec();
+    let hashString: String = String::from_utf8(hashed);
+    return hashString;
 }
