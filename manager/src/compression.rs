5796aac264f0a150879e2dc121fdccb99e466f4f
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2020-07-17 19:26:06 -0300

    doc: removed unused comment section

--- manager/src/compression.rs
+++ manager/src/compression.rs
@@ -26,35 +26,3 @@ pub fn new_tar(file: std::fs::File) -> Builder<std::fs::File> {
 pub fn from_tar(file: std::fs::File) ->  Archive<std::fs::File> {
     Archive::new(file)
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-fn tar_uncompression() {
-    let file = File::open("foo.tar").unwrap();
-    let mut entries = Archive::new(file).entries().unwrap();
-    return entries;
-    for file in a.entries().unwrap() {
-        // Make sure there wasn't an I/O error
-        let mut file = file.unwrap();
-
-        // Inspect metadata about the file
-        println!("{:?}", file.header().path().unwrap());
-        println!("{}", file.header().size().unwrap());
-
-        // files implement the Read trait
-        let mut s = String::new();
-        file.read_to_string(&mut s).unwrap();
-        println!("{}", s);
-    }
-}
-*/
